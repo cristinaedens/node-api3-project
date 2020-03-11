63dad29d511314022b01559e144e576cec1dbbc8
Cristina Edens <edens.cristina@gmail.com>
2020-03-11 15:12:01 -0400

    Set up server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code away!
+const express = require('express');
+const server = express();
+const port = 5000;
+server.use(express.json());
+
+server.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
