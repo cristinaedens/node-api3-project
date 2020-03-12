d948e69f5fcf57e4b8223201dc7452c552cd7581
Cristina Edens <edens.cristina@gmail.com>
2020-03-11 20:41:25 -0400

    met MVP so tired, and frustrated!

--- users/userRouter.js
+++ users/userRouter.js
@@ -3,20 +3,45 @@ const express = require('express');
 const router = express.Router();
 
 const userdb = require('./userDb');
-
+const postdb = require('../posts/postDb');
 router.use("/:id", validateUserId);
 
-router.post('/', (req, res) => {
+router.post('/', validateUser, (req, res) => {
     // do your magic!
+    userdb.insert(req.body)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            console.log('Error:', err);
+            res.status(500).json({ error: "There was an error creating a user." })
+        })
 });
 
-router.post('/:id/posts', (req, res) => {
-    // do your magic!
-
+router.post('/:id/posts', validatePost, (req, res) => {
+    const postId = req.params.id
+    const postInfo = {...req.body, user_id: postId }
+        // do your magic!
+    postdb.insert(postInfo)
+        .then(user => {
+            res.status(201).json(user);
+        })
+        .catch(err => {
+            console.log('Error:', err);
+            res.status(500).json({ error: "There was an error creating a post." })
+        })
 });
 
 router.get('/', (req, res) => {
     // do your magic!
+    userdb.get()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.log("Error: ", err);
+            res.status(500).json({ Error: "the information requested could not be retrieved." });
+        });
 });
 
 router.get('/:id', (req, res) => {
@@ -26,14 +51,41 @@ router.get('/:id', (req, res) => {
 
 router.get('/:id/posts', (req, res) => {
     // do your magic!
+    userdb.getUserPosts(req.params.id)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(err => {
+            console.log('Error:', err)
+            res.status(500).json({ message: "There was a problem retrieving user posts." })
+        })
 });
 
 router.delete('/:id', (req, res) => {
     // do your magic!
+    const id = req.params.id;
+    userdb.remove(id)
+        .then(user => {
+            res.status(200).json({ message: "User was deleted.", user: req.user });
+        })
+        .catch(err => {
+            console.log("Error: ", err)
+            res.status(500).json({ error: "the user id could not be deleted." })
+        })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', validateUser, (req, res) => {
     // do your magic!
+    const id = req.params.id
+
+    userdb.update(id, req.body)
+        .then(user => {
+            res.status(200).json(user)
+        })
+        .catch(err => {
+            console.log('Error:', err)
+            res.status(500).json({ message: "There was a problem updating user ." })
+        })
 });
 
 //custom middleware
@@ -57,11 +109,28 @@ function validateUserId(req, res, next) {
 
 function validateUser(req, res, next) {
     // do your magic!
-}
+    if (req.body) {
+        if (req.body.name) {
+            next();
+        } else {
+            res.status(400).json({ message: "Missing required name field." })
+        }
+    } else {
+        res.status(400).json({ message: "Missing user data." });
+    }
+};
 
 function validatePost(req, res, next) {
     // do your magic!
-
+    if (req.body) {
+        if (req.body.text) {
+            next();
+        } else {
+            res.status(400).json({ message: "Missing required text field." })
+        }
+    } else {
+        res.status(400).json({ message: "Missing post data." });
+    }
 }
 
 module.exports = router;
